fb7e4dc39e7fe546b604592c9fed550854f53fdf
Parkizan <juanmartin.ibarbalz@gmail.com>
2021-08-09 11:57:02 -0300

    nada

---
+++ controles/Assets/Scripts/Agarrar.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agarrar : MonoBehaviour
+{
+    private GameObject pickedObject;
+    bool isHoldingSomething = false;
+    public GameObject handPoint;
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (other.gameObject.tag == "Objeto")
+        {
+            
+            if (Input.GetKeyDown("f") && !isHoldingSomething)
+            {
+                Debug.Log("wee");
+                other.GetComponent<Rigidbody>().useGravity = false;
+                other.GetComponent<Rigidbody>().isKinematic = true;
+                other.transform.position = handPoint.transform.position;
+                other.gameObject.transform.SetParent(handPoint.gameObject.transform);
+                pickedObject = other.gameObject;
+                StartCoroutine(delayCopado());
+                return;
+            }
+            if (Input.GetKeyDown("f") && isHoldingSomething)
+            {
+                Debug.Log("wee22");
+                other.GetComponent<Rigidbody>().useGravity = true;
+                other.GetComponent<Rigidbody>().isKinematic = false;
+                other.transform.position = handPoint.transform.position;
+                other.gameObject.transform.SetParent(handPoint.gameObject.transform);
+                pickedObject = null;
+                StartCoroutine(delayCopado2());
+                return;
+            }
+
+
+        }
+
+
+    }
+
+   
+
+    IEnumerator delayCopado()
+    {
+        yield return new WaitForSeconds(1f);
+        isHoldingSomething = true;
+    }
+
+    IEnumerator delayCopado2()
+    {
+        yield return new WaitForSeconds(1f);
+        isHoldingSomething = false;
+    }
+
+
+
+
+}
