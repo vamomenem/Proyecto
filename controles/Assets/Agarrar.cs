23729427c1a211e818764b39031c0a130a6b0c79
vamomenem <46443780@est.ort.edu.ar>
2021-08-09 11:39:42 -0300

    agarra y suelta

--- controles/Assets/Agarrar.cs
+++ controles/Assets/Agarrar.cs
@@ -4,23 +4,60 @@ using UnityEngine;
 
 public class Agarrar : MonoBehaviour
 {
-    private GameObject pickedObject = null;
+    private GameObject pickedObject;
+    bool isHoldingSomething = false;
     public GameObject handPoint;
-    private void OntriggerStay(Collider other)
+
+    private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.CompareTag("Objeto")){
-            if (Input.GetKey("f") && pickedObject==null){
+
+        if (other.gameObject.tag == "Objeto")
+        {
+            
+            if (Input.GetKeyDown("f") && !isHoldingSomething)
+            {
+                Debug.Log("wee");
                 other.GetComponent<Rigidbody>().useGravity = false;
                 other.GetComponent<Rigidbody>().isKinematic = true;
                 other.transform.position = handPoint.transform.position;
                 other.gameObject.transform.SetParent(handPoint.gameObject.transform);
                 pickedObject = other.gameObject;
-
+                StartCoroutine(delayCopado());
+                return;
+            }
+            if (Input.GetKeyDown("f") && isHoldingSomething)
+            {
+                Debug.Log("wee22");
+                other.GetComponent<Rigidbody>().useGravity = true;
+                other.GetComponent<Rigidbody>().isKinematic = false;
+                other.transform.position = handPoint.transform.position;
+                other.gameObject.transform.SetParent(handPoint.gameObject.transform);
+                pickedObject = null;
+                StartCoroutine(delayCopado2());
+                return;
             }
 
+
         }
+
+
     }
-  
+
+   
+
+    IEnumerator delayCopado()
+    {
+        yield return new WaitForSeconds(1f);
+        isHoldingSomething = true;
+    }
+
+    IEnumerator delayCopado2()
+    {
+        yield return new WaitForSeconds(1f);
+        isHoldingSomething = false;
+    }
+
+
 
 
 }
