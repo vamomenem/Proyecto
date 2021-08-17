a70fae43604343ff0d6dbbfcff0e62eadbf37c57
Parkizan <juanmartin.ibarbalz@gmail.com>
2021-08-17 18:59:56 -0300

    fix 2

--- controles/Assets/Scripts/InstanciadorCajas.cs
+++ controles/Assets/Scripts/InstanciadorCajas.cs
@@ -10,23 +10,7 @@ public class InstanciadorCajas : MonoBehaviour
     
     public GameObject[] spawners;
 
-/*
-    Vector3 posicion1 = new Vector3 (17, 2, 16);
-    Vector3 posicion2 = new Vector3 (15, 2, 16);
-    Vector3 posicion3 = new Vector3 (13, 2, 16);
-    Vector3 posicion4 = new Vector3 (11, 2, 16);
-    Vector3 posicion5 = new Vector3 (9, 2, 16);
-    Vector3 posicion6 = new Vector3 (7, 2, 16);
-    Vector3 posicion7 = new Vector3 (5, 2, 16);
-    Vector3 posicion8 = new Vector3 (3, 2, 16);
-    Vector3 posicion9 = new Vector3 (1, 2, 16);*/
-
     bool hasSpawned = false;
-
-    void Start()
-    {
-        //StartCoroutine(SpawnCajas());
-    }
 
     void Update()
     {
@@ -43,8 +27,7 @@ public class InstanciadorCajas : MonoBehaviour
             {
                 hasSpawned = true;
                 ClonarCajas();
-                yield return new WaitForSeconds(1);
-                //Debug.Log("wee3");
+                yield return new WaitForSeconds(20);
                 hasSpawned = false;
             }
             
@@ -57,52 +40,7 @@ public class InstanciadorCajas : MonoBehaviour
         Debug.Log("Random seed = " + numeroAleatorio);
 
         Instantiate(prefabCaja, spawners[numeroAleatorio].transform.position, spawners[numeroAleatorio].transform.rotation);
-        /*
-        if(numeroAleatorio == 1)
-        {
-            Instantiate(prefabCaja, posicion1, Quaternion.identity);
-        }
-
-        if(numeroAleatorio == 2)
-        {
-            Instantiate(prefabCaja, posicion2, Quaternion.identity);
-        }
-
-        if(numeroAleatorio == 3)
-        {
-            Instantiate(prefabCaja, posicion3, Quaternion.identity);
-        }
-
-        if(numeroAleatorio == 4)
-        {
-            Instantiate(prefabCaja, posicion4, Quaternion.identity);
-        }
-
-        if(numeroAleatorio == 5)
-        {
-            Instantiate(prefabCaja, posicion5, Quaternion.identity);
-        }
-
-        if(numeroAleatorio == 6)
-        {
-            Instantiate(prefabCaja, posicion6, Quaternion.identity);
-        }
-
-        if(numeroAleatorio == 7)
-        {
-            Instantiate(prefabCaja, posicion7, Quaternion.identity);
-        }
-
-        if(numeroAleatorio == 8)
-        {
-            Instantiate(prefabCaja, posicion8, Quaternion.identity);
-        }
-
-        if(numeroAleatorio == 9)
-        {
-            Instantiate(prefabCaja, posicion9, Quaternion.identity);
-        }*/
-
+        
     }
 
 }
