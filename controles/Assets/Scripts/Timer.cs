5e4f7876ed7df900f7df62caa179e4ec64dc0784
vamomenem <46443780@est.ort.edu.ar>
2021-08-19 15:57:09 -0300

    timer

---
+++ controles/Assets/Scripts/Timer.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public GameObject Contador;
+    public int secondsLeft = 30;
+    public bool takingAway = false;
+    public int min = 0;
+
+    void Start ()
+    {
+        Contador.GetComponent<Text>().text = min + ":" + secondsLeft;
+    }
+
+    void Update ()
+    {
+        if (takingAway == false && secondsLeft > 0)
+        {
+            StartCoroutine(TimerTake());
+        }
+    }
+
+    IEnumerator TimerTake()
+    {
+        takingAway = true; 
+        yield return new WaitForSeconds(1);
+        secondsLeft -= 1;
+        if (secondsLeft < 10)
+        {
+              Contador.GetComponent<Text>().text = min + ":0" + secondsLeft;
+        }else
+        {
+             Contador.GetComponent<Text>().text = min + ":" + secondsLeft;
+        }
+        takingAway = false;
+
+        if (secondsLeft == 60 ){
+            min = 1;
+        }
+        if (min == 1 && secondsLeft == 0)
+        {
+            min = 0;
+            secondsLeft = 60;
+        }
+        if (min == 0 && secondsLeft == 0 )
+        {
+            Debug.Log ("gameover");
+        }
+
+
+    }
+
+}
