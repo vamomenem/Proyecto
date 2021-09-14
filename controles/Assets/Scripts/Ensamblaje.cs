ac2c32bfea9bcb04aa88db5120cb5fb0f761b7f5
Parkizan <juanmartin.ibarbalz@gmail.com>
2021-09-14 17:32:03 -0300

    lol

---
+++ controles/Assets/Scripts/Ensamblaje.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ensamblaje : MonoBehaviour
+{
+    public GameObject asamblePoint1;
+    public GameObject asamblePoint2;
+    public GameObject asamblePoint3;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    /*private void OnCollisionEnter(Collider other){
+        if(other.tag == "Objeto"){
+        other.gameObject.transform.position = asamblePoint1.transform.position;
+        }
+    }*/
+    
+}
