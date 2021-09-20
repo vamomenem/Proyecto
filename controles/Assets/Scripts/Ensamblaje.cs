5beee6c02acf068e36ef957d10467ea280d76971
Parkizan <juanmartin.ibarbalz@gmail.com>
2021-09-20 15:47:30 -0300

    owo

--- controles/Assets/Scripts/Ensamblaje.cs
+++ controles/Assets/Scripts/Ensamblaje.cs
@@ -4,24 +4,46 @@ using UnityEngine;
 
 public class Ensamblaje : MonoBehaviour
 {
+    public Transform mueble;
     public GameObject asamblePoint1;
     public GameObject asamblePoint2;
     public GameObject asamblePoint3;
 
-    void Start()
-    {
-        
-    }
+    int indexOfSlots = 1;
 
-    void Update()
-    {
-        
-    }
 
-    /*private void OnCollisionEnter(Collider other){
+    private void OnCollisionEnter(Collision other){
+        Debug.Log(other.gameObject);
+        if (other.gameObject.tag == "Objeto" && other.transform.childCount > 0){
+            switch (indexOfSlots){
+                case 1:
+                indexOfSlots++;
+                    other.transform.position = asamblePoint1.transform.position;
+                    break;
+                case 2:
+                
+                indexOfSlots++;
+                    other.transform.position = asamblePoint2.transform.position;
+                    break;
+                case 3:
+                
+                    other.transform.position = asamblePoint3.transform.position;
+                    //craft
+                    break;
+
+
+            }
+        }
+
+        /*if(other.transform.childCount > 0){
+        mueble = other.gameObject.transform.GetChild(0);
+        mueble = asamblePoint1.transform;
+        other.gameObject.transform.SetParent(asamblePoint1.transform);
+        }
+
         if(other.tag == "Objeto"){
         other.gameObject.transform.position = asamblePoint1.transform.position;
-        }
-    }*/
+        }*/
+    }
     
 }
