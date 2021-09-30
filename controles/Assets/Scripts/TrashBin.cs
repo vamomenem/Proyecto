73981b215bbdd62b98f11d4d65991a547e8a28d8
vamomenem <46443780@est.ort.edu.ar>
2021-09-30 14:03:15 -0300

    TrashBin

---
+++ controles/Assets/Scripts/TrashBin.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashBin : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider coll) 
+    {
+        if (coll.transform.parent == null && coll.gameObject.tag == "Objeto"){
+           
+           Destroy(gameObject);
+        }
+    }
+}
